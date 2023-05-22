e9e94fc8dacc945b2b9fffd0561b6303b675f7bc
Javier <Ledesmajavier2019@gmail.com>
2023-05-22 15:45:54 -0300

    cambios minimos

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8800
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`¡Servidor en ejecución! PORT: ${PORT}`)
 })
