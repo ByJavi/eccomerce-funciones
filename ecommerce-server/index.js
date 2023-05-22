1d92281ced13b385cb5ded4ed27ed708493928a0
Javier <Ledesmajavier2019@gmail.com>
2023-05-22 15:34:50 -0300

    configuracion Eslint, Prettier y inicio del server con Express

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -1,1 +1,8 @@
-console.log("Start!")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8800
+
+app.listen(PORT, ()=> {
+    console.log(`¡Servidor en ejecución! PORT: ${PORT}`)
+})
