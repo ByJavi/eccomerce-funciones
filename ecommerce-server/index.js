d10768623e72a13156fc56c6f1337a95471a8111
Javier <Ledesmajavier2019@gmail.com>
2023-05-22 16:25:59 -0300

    Conexion con MONGODB

--- ecommerce-server/index.js
+++ ecommerce-server/index.js
@@ -1,8 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 8800
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8800;
+
+const dbConnect = require("./conection/dbConnect");
+dbConnect();
+
+app.use("/", (req, res) => {
+  res.send("Index");
+});
 
 app.listen(PORT, () => {
-    console.log(`¡Servidor en ejecución! PORT: ${PORT}`)
-})
+  console.log(`¡Servidor en ejecución! PORT: ${PORT}`);
+});
